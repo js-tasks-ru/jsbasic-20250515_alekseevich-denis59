44520e3fa0afb495d2b81f030125f65f0ac3232a
Яшин Денис Алексеевич <asindenisalekseevic@Asins-MacBook-Pro.local>
2025-05-19 21:27:33 +0300

    Выполнил все задания из занятий  0 и 1

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,12 @@
 function checkSpam(str) {
-  // ваш код...
-}
+    str = str.toLowerCase();
+    if ( 
+        str.includes('1xbet') ||
+        str.includes('xxx')
+    ) {
+        return true;
+    }
+    else {
+        return false
+    };
+};
