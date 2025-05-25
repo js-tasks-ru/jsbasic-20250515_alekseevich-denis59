091c9e69bed007f1598276a94ae2a5b14b4705ce
Яшин Денис Алексеевич <asindenisalekseevic@Asins-MacBook-Pro.local>
2025-05-25 15:18:10 +0300

    Решил все задания из второго модуля

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,25 @@
-let calculator = {
-  // ваш код
-};
+function getNumberFromUser(numberOfValue) {
+    let num;
+    do {
+        num = prompt(`Введите ${numberOfValue} (только положительные числа):`, 0);
+        num = Number(num);
+    } while ( isNaN(num) || num === ''|| num < 0);
+    return num;
+}
 
+let calculator = {
+	read() {
+		this.value1 = getNumberFromUser(1);
+		this.value2 = getNumberFromUser(2);
+	},
+    sum(){
+    return this.value1 + this.value2;
+    },
+    mul() {
+        return this.value1 * this.value2;
+    },
+}
+
+calculator.read()
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
