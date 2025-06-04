6adac1cc37f00ecc5c8c535dd9dc15c5468c48ee
Яшин Денис Алексеевич <asindenisalekseevic@Asins-MacBook-Pro.local>
2025-06-04 21:53:32 +0300

    Внес правки по комментариям из прошлых коммитов. Выполнил задания из третьего модуля.

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,12 +1,4 @@
 function checkSpam(str) {
     str = str.toLowerCase();
-    if ( 
-        str.includes('1xbet') ||
-        str.includes('xxx')
-    ) {
-        return true;
-    }
-    else {
-        return false
-    };
+    return str.includes('1xbet') || str.includes('xxx');
 };
--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,5 @@
 function showSalary(users, age) {
-  // ваш код...
+  let filteredUsers = users.filter(user => user.age <= age);
+  let result = filteredUsers.map(user => `${user.name}, ${user.balance}`).join('\n');
+  return result;
 }
